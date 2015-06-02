c3caa73268a04640b4ead551bd979e5abbfd567f
B. Landau <landissure@outlook.com>
2015-06-02 01:52:31 -0700

    New file
    Added new task holder. Damned structlessness.

---
+++ SpacesuitsTasks.java
@@ -0,0 +1,92 @@
+package landissure.mc.lscpspacesuits;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+
+public class SpacesuitsTasks {
+	
+	public static Runnable armor = new Runnable(){
+		public void run() {
+			for(Player g : Bukkit.getServer().getOnlinePlayers()){
+				ItemStack helm = g.getInventory().getHelmet();
+				if(helm != null && ItemTools.compareIgnoreDurability(helm, new ItemStack(Material.IRON_HELMET)) && g.getWorld().getName().equals("space") ){
+					helm.setDurability((short)  (helm.getDurability()+1));
+					g.getInventory().setHelmet(helm);
+					if(g.getInventory().getHelmet().getDurability() > Material.IRON_HELMET.getMaxDurability()){
+						g.getInventory().setHelmet(null);
+					}
+				}
+			}
+		}
+	};
+	
+	private static boolean oxySpace(Player p){
+		if(p.getWorld().getName().equals("space")){
+			return false;
+		}
+		else{
+		return 	LSCPspacesuits.planets.get(p.getWorld().getName()).isOxygenated();
+		}
+	}
+
+	public static Runnable planetBoundaryActor = new Runnable() {
+		public void run(){
+			for(Player p : Bukkit.getServer().getWorld("space").getPlayers()){
+				for(PlanetHandler x : LSCPspacesuits.planets.values()){
+					if(x.inPortal(p.getLocation())){
+						p.teleport(x.getEntry());
+					}
+				}
+			}
+		}
+	};
+
+
+	public static Runnable flyInSpace = new Runnable(){
+
+		public void run() {
+			for(Player g : Bukkit.getServer().getOnlinePlayers()){
+				if(g.getWorld().getName().equals("space")){
+					g.setAllowFlight(true);
+				}
+				else if(!(g.getGameMode() == GameMode.CREATIVE)){
+					g.setAllowFlight(false);
+				}
+			}
+		}
+	};
+
+	public static Runnable spacehurt = new Runnable() {
+
+		public void run() {
+			for(Player p : Bukkit.getServer().getOnlinePlayers()){
+				if(p.getInventory().getHelmet() == null && !p.getGameMode().equals(GameMode.CREATIVE) &&  !oxySpace(p)){
+					try{
+						p.setHealth(p.getHealth()-1);
+						if(p.getHealth() <= 1){
+							p.setHealth(0);
+						}
+					}catch(IllegalArgumentException e){
+					}
+				}
+			}
+		}
+
+	};
+	
+	public static Runnable exitAtmosphereDetector = new Runnable(){
+		public void run(){
+			for(Player p : Bukkit.getServer().getOnlinePlayers()){
+				if(!p.getWorld().getName().equals("space") && p.getLocation().getY() > 256){
+					p.teleport(LSCPspacesuits.planets.get(p.getWorld().getName()).getExit());
+				}
+			}
+		}
+	};
+}
+
+
