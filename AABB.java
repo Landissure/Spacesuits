1d3f628ad37ebe014625235ffce0356338b7809b
B. Landau <landissure@outlook.com>
2015-06-02 22:16:10 -0700

    Now with block detectors
    Just that.

--- AABB.java
+++ AABB.java
@@ -1,14 +1,15 @@
 package landissure.mc.lscpspacesuits;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 
 public class AABB {
 	private Location pin1;
 	private Location pin2;
 
 	public AABB(Location o, Location t){
-		pin1 = o;
-		pin2 = t;
+		pin1 = o.getBlock().getLocation();
+		pin2 = t.getBlock().getLocation();
 	}
 	
 	public boolean isWithin(Location f){
@@ -16,6 +17,11 @@ public class AABB {
 				(f.getY() > pin1.getY() && f.getY() < pin2.getY()) &&
 				(f.getZ() > pin1.getZ() && f.getZ() < pin2.getZ()) &&
 				f.getWorld().getName().equals(pin1.getWorld().getName());
+	}
+	
+	public boolean isWithin(Material m){
+		Location d = new Location(pin1.getWorld(),pin1.getX()+5,pin1.getY()+5,pin1.getZ()+5);
+		return d.getBlock().getType().equals(m);
 	}
 	
 	public Location getPin1(){
